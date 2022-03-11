69c9997365a49432a1fe200f9139a02aeb37b592
BertVanAcker <bert.van.acker@hotmail.nl>
2022-03-11 12:06:59 +0100

    main.blocks, custom.ts bijwerken

--- custom.ts
+++ custom.ts
@@ -31,6 +31,7 @@ namespace custom {
      * TODO: describe your function here
      * @param value describe value here, eg: 5
      */
+    //%color=##f73b26 
     //% block
     export function fib(value: number): number {
         return value <= 1 ? value : fib(value -1) + fib(value - 2);
